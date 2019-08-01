dd9ec1fe1341cc2eaf364ea3ed8a12c633a6d8f2
unknown <sagaraher8201@gmail.com>
2019-08-02 03:41:11 +0530

    Added log4j file

--- src/main/java/com/demouat/qa/base/BaseClass.java
+++ src/main/java/com/demouat/qa/base/BaseClass.java
@@ -1,6 +1,5 @@
 package com.demouat.qa.base;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,25 +7,39 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class BaseClass {
 	
-	public static Properties prop;
-	public static WebDriver driver;
+	Properties prop;
+	WebDriver driver;
 	
-	public BaseClass() throws IOException {
+	public BaseClass() throws IOException{
 		
-		System.setProperty("webdriver.chrome.driver",prop.getProperty("driverPath"));					
-	    driver = new ChromeDriver();					
-	    String url = prop.getProperty("url");			
-	    driver.manage().window().maximize();
-	    
-	    prop = new Properties();
-	    File f = new File(System.getProperty("user.dir")+"src\\main\\java\\com\\demoaut\\qa\\properties\\config.properties");
-	    FileInputStream fileInputStream = new FileInputStream(f);									
-	    prop.load(fileInputStream);
+		System.setProperty("webdriver.chrome.driver",prop.getProperty("driverPath"));
 		
+		prop = new Properties();
+		FileInputStream ips = new FileInputStream(System.getProperty("user.dir")+"src/main/java/com/demoaut/qa/properties/config.properties");
+		prop.load(ips);
+		
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")){
+			driver = new ChromeDriver();
+		}
+		else if(browserName.equals("ff")){
+			driver = new FirefoxDriver();
+		}
+		else {
+			System.out.println("browser value not defined");
+		}
+		
+		driver.get(prop.getProperty("url"));
 		
 	}
+	
 
+
+	
 }
